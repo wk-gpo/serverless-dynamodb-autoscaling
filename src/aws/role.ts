ca6d799f594c43de69579fd622512b76dd756b58
Adam Cuper <adam.cuper@ardentcode.com>
2020-01-28 19:40:49 +0100

    Code formatting

--- src/aws/role.ts
+++ src/aws/role.ts
@@ -1,77 +1,82 @@
 import Resource from './resource'
 
 export default class Role extends Resource {
-  private readonly type: string = 'AWS::IAM::Role'
-  private readonly version: string = '2012-10-17'
-  private readonly actions = {
-    CloudWatch: [
-      'cloudwatch:PutMetricAlarm',
-      'cloudwatch:DescribeAlarms',
-      'cloudwatch:DeleteAlarms',
-      'cloudwatch:GetMetricStatistics',
-      'cloudwatch:SetAlarmState'
-    ],
-    DynamoDB: [
-      'dynamodb:DescribeTable',
-      'dynamodb:UpdateTable'
-    ]
-  }
+    private readonly type: string = 'AWS::IAM::Role'
+    private readonly version: string = '2012-10-17'
+    private readonly actions = {
+        CloudWatch: [
+            'cloudwatch:PutMetricAlarm',
+            'cloudwatch:DescribeAlarms',
+            'cloudwatch:DeleteAlarms',
+            'cloudwatch:GetMetricStatistics',
+            'cloudwatch:SetAlarmState'
+        ],
+        DynamoDB: ['dynamodb:DescribeTable', 'dynamodb:UpdateTable']
+    }
 
-  constructor (
-    options: Options
-  ) { super(options) }
+    constructor(options: Options) {
+        super(options)
+    }
 
-  public toJSON(): any {
-    const RoleName = this.name.role()
-    const PolicyName = this.name.policyRole()
+    public toJSON(): any {
+        const RoleName = this.name.role()
+        const PolicyName = this.name.policyRole()
 
-    const DependsOn = [ this.options.table ].concat(this.dependencies)
-    const Principal = this.principal()
-    const Version = this.version
-    const Type = this.type
+        const DependsOn = [this.options.table].concat(this.dependencies)
+        const Principal = this.principal()
+        const Version = this.version
+        const Type = this.type
 
-    return {
-      [RoleName]: {
-        DependsOn,
-        Properties: {
-          AssumeRolePolicyDocument: {
-            Statement: [
-              { Action: 'sts:AssumeRole', Effect: 'Allow', Principal }
-            ],
-            Version
-          },
-          Policies: [
-            {
-              PolicyDocument: {
-                Statement: [
-                  { Action: this.actions.CloudWatch, Effect: 'Allow', Resource: '*' },
-                  { Action: this.actions.DynamoDB, Effect: 'Allow', Resource: this.resource() }
-                ],
-                Version
-              },
-              PolicyName
+        return {
+            [RoleName]: {
+                DependsOn,
+                Properties: {
+                    AssumeRolePolicyDocument: {
+                        Statement: [{ Action: 'sts:AssumeRole', Effect: 'Allow', Principal }],
+                        Version
+                    },
+                    Policies: [
+                        {
+                            PolicyDocument: {
+                                Statement: [
+                                    { Action: this.actions.CloudWatch, Effect: 'Allow', Resource: '*' },
+                                    { Action: this.actions.DynamoDB, Effect: 'Allow', Resource: this.resource() }
+                                ],
+                                Version
+                            },
+                            PolicyName
+                        }
+                    ],
+                    RoleName
+                },
+                Type
             }
-          ],
-          RoleName
-        },
-        Type
-      }
+        }
     }
-  }
 
-  private resource(): {} {
-    let arnAwsPrefix = 'aws';
-    if (this.options.region.indexOf('cn-') === 0) {
-      arnAwsPrefix += '-cn';
+    private resource(): {} {
+        let arnAwsPrefix = 'aws'
+        if (this.options.region.indexOf('cn-') === 0) {
+            arnAwsPrefix += '-cn'
+        }
+        return {
+            'Fn::Join': [
+                '',
+                [
+                    'arn:',
+                    { Ref: arnAwsPrefix },
+                    ':dynamodb:*:',
+                    { Ref: 'AWS::AccountId' },
+                    ':table/',
+                    { Ref: this.options.table }
+                ]
+            ]
+        }
     }
-    return {
-      'Fn::Join': [ '', [ 'arn:', { Ref: arnAwsPrefix}, ':dynamodb:*:', { Ref: 'AWS::AccountId' }, ':table/', { Ref: this.options.table } ] ]
-    }
-  }
 
-  private principal(): {} {
-    return {
-      Service: 'application-autoscaling.amazonaws.com'
+    private principal(): {} {
+        return {
+            Service: 'application-autoscaling.amazonaws.com'
+        }
     }
-  }
 }
